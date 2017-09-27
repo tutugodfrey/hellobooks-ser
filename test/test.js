8c0c3a27da404fb0efc7b0623dbab2ef89e2ab2e
tutu godfrey <godfrey_tutu@yahoo.com>
2017-09-27 11:42:42 +0100

    correct url path for index.js on container

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
 
-const app= require("../lib-cov");
+//const app= require("../lib-cov");
 const assert = require("chai").assert;
-const main = require("../controllers/index.js");
+const main = require("../server/controllers/index.js");
